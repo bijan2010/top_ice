83f2a0ea86e3358186d1aeb36be77f80c60f72cd
bijan2010 <bamirazodi79@gmail.com>
2024-09-05 16:24:56 -0700

    Add build filestdd

---
+++ build/precache-manifest.32360e748bf14d59f0b8f25382d9ce93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fc175c6949f298ab7f2570b62b3ce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db7a3e57fffa09f10ec",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9633f41c7f79f1709db8",
+    "url": "/static/js/2.12627ea4.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "4db7a3e57fffa09f10ec",
+    "url": "/static/js/main.73b9816b.chunk.js"
+  },
+  {
+    "revision": "e21c8a56d64ede50141d",
+    "url": "/static/js/runtime~main.891e0007.js"
+  }
+]);
